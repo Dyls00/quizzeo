796cd6c0c97c1ff7ce47dad5e574f9618fe06e42
Dyls <Johane-Arlis.NSANGOUAKANDA@estiam.com>
2025-11-27 11:37:40 +0100

    Ajout modules

---
+++ app/quizz/[id]/play/page.tsx
@@ -0,0 +1,13 @@
+export default function Play() {
+    return (
+        <div className="question-main">
+            <div className="question-form mt-30">
+                <form className="form form-question">
+                    <div className="question-text">Bienvenue</div>
+                    <label className="question-title">Veuillez valider votre quizz</label>
+                    <button className="button-confirm">Confirm !</button>
+                </form>
+            </div>
+        </div>
+    )
+}
